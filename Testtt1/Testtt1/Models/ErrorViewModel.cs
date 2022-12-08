b0eec54ec4c1f34305919c90d0f241dbff9ea93d
kobicohen <kobicohenv1.1@gmail.com>
2022-12-08 17:16:06 +0200

    Add Project

---
+++ Testtt1/Testtt1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Testtt1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
