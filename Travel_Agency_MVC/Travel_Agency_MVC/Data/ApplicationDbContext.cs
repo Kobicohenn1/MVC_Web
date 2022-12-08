bb723add9e8966f0fc25a5b5039c378dfaf26269
Pavel Savelev <Pavelsav960@gmail.com>
2022-12-08 17:57:13 +0200

    added connection string + new data folder

---
+++ Travel_Agency_MVC/Travel_Agency_MVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Travel_Agency_MVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        //Here starts the model part , at the moment there are no models available so this line is in comment
+        //public DbSet<>
+    }
+}
