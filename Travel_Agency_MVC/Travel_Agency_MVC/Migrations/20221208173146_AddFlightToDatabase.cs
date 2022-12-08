eb47f64f109ce1b5ad0d96982c7b4ceebea5f644
kobicohen <kobicohenv1.1@gmail.com>
2022-12-08 19:37:30 +0200

    Add_FlightModel + Connection String
    Define DbContext FlightModel

---
+++ Travel_Agency_MVC/Travel_Agency_MVC/Migrations/20221208173146_AddFlightToDatabase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Travel_Agency_MVC.Migrations
+{
+    public partial class AddFlightToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Flights",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FlightDestiantion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FlightOrigin = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FlightDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Price = table.Column<float>(type: "real", nullable: false),
+                    FlightSeats = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Flights", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Flights");
+        }
+    }
+}
