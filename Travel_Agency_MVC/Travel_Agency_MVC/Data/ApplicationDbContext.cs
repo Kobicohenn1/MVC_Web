76043a09adffaa96c03866d94c4756147fc07117
Pavel Savelev <Pavelsav960@gmail.com>
2022-12-10 18:59:55 +0200

    minor change
    added using statement mainly

--- Travel_Agency_MVC/Travel_Agency_MVC/Data/ApplicationDbContext.cs
+++ Travel_Agency_MVC/Travel_Agency_MVC/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Travel_Agency_MVC.Models;
 
 namespace Travel_Agency_MVC.Data
