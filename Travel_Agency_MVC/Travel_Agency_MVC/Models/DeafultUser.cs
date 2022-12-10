78aed88a6b89b2be62b42a55404f3cea804c6ead
Pavel Savelev <Pavelsav960@gmail.com>
2022-12-10 19:39:38 +0200

    identity added , before migration

--- Travel_Agency_MVC/Travel_Agency_MVC/Models/DeafultUser.cs
+++ Travel_Agency_MVC/Travel_Agency_MVC/Models/DeafultUser.cs
@@ -1,12 +1,19 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Travel_Agency_MVC.Models
 {
-    public class DeafultUser : IdentityUser
+    public class DefaultUser : IdentityUser
     {
+        [PersonalData]
+        [Column(TypeName ="navchar(100)")]
+        public string FirstName { get; set; }
+        [PersonalData]
+        [Column(TypeName = "navchar(100)")]
+        public string LastName { get; set; }
     }
 }
--- Travel_Agency_MVC/Travel_Agency_MVC/Program.cs
+++ Travel_Agency_MVC/Travel_Agency_MVC/Program.cs
@@ -1,14 +1,20 @@
 using Microsoft.EntityFrameworkCore;
 using Travel_Agency_MVC.Data;
+using Microsoft.AspNetCore.Identity;
+using Travel_Agency_MVC.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
+
+builder.Services.AddDefaultIdentity<DefaultUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
 
